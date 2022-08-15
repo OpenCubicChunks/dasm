4b378b37b07353223a5bba6473223e41619ad800
Tom Martin <tom.martin1239@gmail.com>
2022-08-15 18:57:52 +0100

    Add identity mappings provider by default

--- src/main/java/io/github/opencubicchunks/dasm/MappingsProvider.java
+++ src/main/java/io/github/opencubicchunks/dasm/MappingsProvider.java
@@ -1,6 +1,23 @@
 package io.github.opencubicchunks.dasm;
 
 public interface MappingsProvider {
+    MappingsProvider IDENTITY = new MappingsProvider() {
+        @Override
+        public String mapFieldName(String namespace, String owner, String fieldName, String descriptor) {
+            return fieldName;
+        }
+
+        @Override
+        public String mapMethodName(String namespace, String owner, String methodName, String descriptor) {
+            return methodName;
+        }
+
+        @Override
+        public String mapClassName(String namespace, String className) {
+            return className;
+        }
+    };
+
     String mapFieldName(String namespace, String owner, String fieldName, String descriptor);
 
     String mapMethodName(String namespace, String owner, String methodName, String descriptor);
